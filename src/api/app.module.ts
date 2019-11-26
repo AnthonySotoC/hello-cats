3e9c5d9aba574a977102891c8622a48162bfb411
Anthony Soto <anthony@firstfactory.com>
2019-11-26 07:58:56 -0600

    add the config service

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { CatModule } from './cat/cat.module';
 import { HumanModule } from './human/human.module';
-import { GRAPHQL_CONFIG } from 'src/shared/config/graphql';
+import { GRAPHQL_CONFIG } from 'src/shared/config/module-options/graphql';
+import { ConfigService } from 'src/shared/config/config.service';
+import { ConfigModule } from 'src/shared/config/config.module';
 
 @Module({
-  imports: [CatModule, HumanModule, GraphQLModule.forRoot(GRAPHQL_CONFIG)],
+  imports: [
+    CatModule,
+    HumanModule,
+    ConfigModule,
+    GraphQLModule.forRoot(GRAPHQL_CONFIG),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(config: ConfigService) {
+    console.log(config.get('PORT'));
+  }
+}
