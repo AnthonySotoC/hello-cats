ca2e49699966b0c849bcf8425691b36da7cccf59
FF_Antony <ff_antony@FF-Admins-MacBook-Pro.local>
2020-01-02 17:25:23 -0600

    Resolve the testing issues

--- src/api/cat/test/cat.module.mock.ts
+++ src/api/cat/test/cat.module.mock.ts
@@ -1,17 +1,24 @@
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Reflector } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
 
 import { Cat } from '@shared/datasource/model/cat.entity';
 import { CatService } from '@api/cat/cat.service';
 import { CatResolver } from '@api/cat/cat.resolver';
+import { ConfigService } from '@shared/config/config.service';
 
 export default Test.createTestingModule({
+  imports: [Reflector],
   providers: [
     CatResolver,
     CatService,
     {
       provide: getRepositoryToken(Cat),
       useValue: {},
+    },
+    {
+      provide: ConfigService,
+      useValue: new ConfigService('.env.development'),
     },
   ],
   exports: [CatService],
